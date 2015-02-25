04810c287f666c13bce5dc4c42af71dcd4cbeb19
Eirik Jakobsen <eirikjak@gmail.com>
2015-02-25 04:02:28 +0100

    script for initdb and server start

---
+++ manage.py
@@ -0,0 +1,17 @@
+from flask_script import Manager
+from flask import Flask
+from source.models import Base,engine
+from source.server import app
+manager = Manager(app)
+
+
+@manager.command
+def initdb():
+    Base.metadata.create_all(bind=engine)
+
+@manager.command
+def runserver():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    manager.run()
